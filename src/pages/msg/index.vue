bda38af28979e5abd1377e8a3786a686565cd513
young <ly532265997@gmail.com>
2018-03-29 15:13:12 +0800

    add msg example

---
+++ src/pages/msg/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="page">
+    <div class="page__hd">
+      <div class="page__title">Msg</div>
+      <div class="page__desc">提示页</div>
+    </div>
+    <div class="page__bd">
+      <div class="weui-btn-area">
+        <button
+          @click="openSuccess"
+          class="weui-btn"
+          type="default"
+        >
+          成功提示页
+        </button>
+        <button
+          @click="openFail"
+          class="weui-btn"
+          type="default"
+        >
+          失败提示页
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    openSuccess() {
+      wx.navigateTo({
+        url: '/pages/msg_success/msg_success',
+      });
+    },
+    openFail() {
+      wx.navigateTo({
+        url: '/pages/msg_fail/msg_fail',
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less">
+page {
+  background-color: #ffffff;
+}
+</style>
+
+
+
---
+++ src/pages/msg/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import App from './index';
+
+new Vue(App).$mount();
+
